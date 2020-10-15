9b19704c37141b5cd12f9d12029921826002e250
Ayaz Araf <ayaz.araf@gmail.com>
2020-10-15 12:40:20 -0400

    Update palindrome-checker_longstring.js

--- palindrome-checker_longstring.js
+++ palindrome-checker_longstring.js
@@ -3,7 +3,7 @@ function palindrome(str) {
 var f= 0; //this is variable is set to 0 and to be increment 
 var b=str.length-1; //this variable for str written in reverse.
     while(b>f){// 
-  if(str[f].match(/[\W_]/))//loop to match non word characters
+  if(str[f].match(/[\W_]/))//loop to match word characters
   {
     f++;
     continue;
