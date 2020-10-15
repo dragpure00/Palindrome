e06041f9033159bcee18f5093a5745e6721cda28
Ayaz Araf <ayaz.araf@gmail.com>
2020-10-15 12:15:56 -0400

    Update palinedrome_basic.js
    added comments

--- palinedrome_basic.js
+++ palinedrome_basic.js
@@ -1,5 +1,5 @@
-function palindrome(str) {
-var p = str.replace(/[\W_]/gi,'').toLowerCase();
+function palindrome(str) {// same method as you would check for long strings except using the replace method to replace any nonword charactter as space
+var p = str.replace(/[\W_]/gi,'').toLowerCase();//This is only useful if you want to check for short strings.
 var forward=0;
 var back = p.length-1;
   while(back>forward){
